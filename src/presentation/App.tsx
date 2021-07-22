7adb1353dcb4c6835bfd7b736cd2c6c568899048
s.igarashi <50ra4.dev.dev@gmail.com>
2021-07-22 23:45:50 +0900

    apply: styled-components to App

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -17,5 +18,24 @@ function App() {
     </div>
   );
 }
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    & {
+      animation: ${spin} infinite 20s linear;
+    }
+  }
+`;
 
 export default App;

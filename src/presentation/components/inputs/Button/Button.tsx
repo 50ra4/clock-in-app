67caeb6a93fff524dcbe7534a47d46dde334c614
s.igarashi <50ra4.dev.dev@gmail.com>
2021-07-23 18:40:47 +0900

    add: Button component sample

---
+++ src/presentation/components/inputs/Button/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+type OwnProps = {
+  fullWidth?: boolean;
+};
+type InheritedProps = Omit<JSX.IntrinsicElements['button'], keyof OwnProps>;
+
+export type ButtonProps = OwnProps & InheritedProps;
+
+export const UnStyledButton = React.memo(function Button({ fullWidth = false, children, ...otherProps }: ButtonProps) {
+  return <button {...otherProps}>{children}</button>;
+});
+
+export const Button = styled(UnStyledButton)`
+  ${({ fullWidth }) =>
+    !!fullWidth &&
+    css`
+      width: 100%;
+    `}
+`;

ab3f8c18e2a18b3eeb1a94e5094426299468523a
50ra4 <50ra4.dev.dev@gmail.com>
2021-07-25 17:23:42 +0900

    add: Authentication thunk

---
+++ src/thunks/authentication.ts
@@ -0,0 +1,40 @@
+import * as authenticationService from 'services/authentication';
+import { ThunkAction } from 'redux-thunk';
+import { Dispatch } from '@reduxjs/toolkit';
+import { AppState } from 'store/root';
+import { AuthenticationActions, authenticationActions } from 'store/authentication';
+import { initializeState, RootActions } from 'store/rootActions';
+
+export const signIn =
+  (email: string, password: string): ThunkAction<void, AppState, undefined, AuthenticationActions> =>
+  async (dispatch: Dispatch<AuthenticationActions>) => {
+    dispatch(authenticationActions.loggingIn());
+    await authenticationService
+      .signIn(email, password)
+      .then((user) => {
+        dispatch(authenticationActions.success({ emailVerified: !!user?.emailVerified }));
+      })
+      .catch((err) => {
+        dispatch(authenticationActions.failed(err));
+      });
+  };
+
+export const signUp =
+  (email: string, password: string): ThunkAction<void, AppState, undefined, AuthenticationActions> =>
+  async (dispatch: Dispatch<AuthenticationActions>) => {
+    dispatch(authenticationActions.loggingIn());
+    await authenticationService
+      .signUp(email, password)
+      .then((user) => {
+        dispatch(authenticationActions.success({ emailVerified: !!user?.emailVerified }));
+      })
+      .catch((err) => {
+        dispatch(authenticationActions.failed(err));
+      });
+  };
+
+export const signOut =
+  (): ThunkAction<void, AppState, undefined, RootActions> => async (dispatch: Dispatch<RootActions>) => {
+    await authenticationService.signOut();
+    dispatch(initializeState());
+  };

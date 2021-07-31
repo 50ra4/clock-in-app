6dc1548553c83963f72b4753b44f4b002446dbd4
50ra4 <50ra4.dev.dev@gmail.com>
2021-07-31 19:24:06 +0900

    add: Backdrop component

---
+++ src/presentation/components/feedback/Backdrop/Backdrop.tsx
@@ -0,0 +1,53 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+
+type OwnProps = {
+  className?: string;
+  open?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  children?: React.ReactNode;
+};
+
+export type BackdropProps = OwnProps;
+
+const UnStyledBackdrop = React.memo(function Backdrop({
+  open = false,
+  onClick,
+  children = null,
+  className,
+  ...otherProps
+}: BackdropProps) {
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      event.preventDefault();
+      if (onClick) {
+        onClick(event);
+      }
+    },
+    [onClick],
+  );
+
+  if (!open) {
+    return null;
+  }
+
+  return (
+    <div {...otherProps} className={className} area-hidden="true" role="none" onClick={handleClick}>
+      {children}
+    </div>
+  );
+});
+
+export const Backdrop = styled(UnStyledBackdrop)`
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-tap-highlight-color: transparent;
+  z-index: ${({ theme }) => theme.zIndex.drawer + 1};
+`;

454e844d96e6be598778f906a63a88645b9bba12
50ra4 <50ra4.dev.dev@gmail.com>
2021-08-01 02:11:42 +0900

    fix: LoadingGuard

--- src/presentation/components/feedback/LoadingGuard/LoadingGuard.tsx
+++ src/presentation/components/feedback/LoadingGuard/LoadingGuard.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import { Backdrop } from '../Backdrop/Backdrop';
 import { LoadingSpinner } from '../LoadingSpinner/LoadingSpinner';
 
 type OwnProps = {
@@ -18,37 +19,11 @@ const UnStyledLoadingGuard = React.memo(function LoadingGuard({
   message,
   ...otherProps
 }: LoadingGuardProps) {
-  const handleClick = useCallback(
-    (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      event.preventDefault();
-      if (onClick) {
-        onClick(event);
-      }
-    },
-    [onClick],
-  );
-
-  if (!open) {
-    return null;
-  }
-
   return (
-    <div {...otherProps} className={className} area-hidden="true" role="none" onClick={handleClick}>
+    <Backdrop {...otherProps} open={open} className={className} onClick={onClick}>
       <LoadingSpinner iconSize="extraLarge" message={message} />
-    </div>
+    </Backdrop>
   );
 });
 
-export const LoadingGuard = styled(UnStyledLoadingGuard)`
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  position: fixed;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(0, 0, 0, 0.5);
-  -webkit-tap-highlight-color: transparent;
-  z-index: ${({ theme }) => theme.zIndex.drawer + 1};
-`;
+export const LoadingGuard = styled(UnStyledLoadingGuard)``;

fcc6b4b2b88fd449181b25e478525ec076d94323
50ra4 <50ra4.dev.dev@gmail.com>
2021-07-31 12:29:31 +0900

    add: ResponsiveLayout component

---
+++ src/presentation/components/display/ResponsiveLayout/ResponsiveLayout.stories.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+import { ResponsiveLayout } from './ResponsiveLayout';
+import { createStoryMeta, createStoryTemplate } from 'utils/storybookUtils';
+
+export default createStoryMeta(ResponsiveLayout, {
+  title: 'display/ResponsiveLayout',
+});
+
+const Content = styled.div`
+  height: 200px;
+  background-color: palegreen;
+`;
+
+const Template = createStoryTemplate(ResponsiveLayout);
+export const Docs = Template.bind({});
+Docs.args = {
+  children: <Content>Responsive</Content>,
+};

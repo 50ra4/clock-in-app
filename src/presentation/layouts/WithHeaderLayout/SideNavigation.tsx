6b237d72472ddd2bb2c31f3c28251005b8ace5ff
50ra4 <50ra4.dev.dev@gmail.com>
2021-08-13 22:33:06 +0900

    add: SideNavigation component

---
+++ src/presentation/layouts/WithHeaderLayout/SideNavigation.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import { PAGE_PATH } from 'constants/path';
+import { Backdrop } from 'presentation/components/feedback/Backdrop/Backdrop';
+import { headerHeight } from 'presentation/components/surfaces/Header/Header';
+
+type OwnProps = {
+  className?: string;
+  open: boolean;
+  onClose: () => void;
+};
+
+/**
+ * @see https://www.w3schools.com/howto/howto_js_sidenav.asp
+ */
+export const SideNavigation = React.memo(function SideNavigation({ className, open, onClose }: OwnProps) {
+  return (
+    <StyledBackdrop className={className} open={open} onClick={onClose}>
+      <StyledRoot>
+        <nav>
+          <ul>
+            <li>
+              <Link to={PAGE_PATH.top}>
+                <button>Top</button>
+              </Link>
+            </li>
+            <li>
+              <Link to={PAGE_PATH.login}>
+                <button>ログイン</button>
+              </Link>
+            </li>
+            <li>
+              <Link to={PAGE_PATH.registration}>
+                <button>アカウント登録</button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </StyledRoot>
+    </StyledBackdrop>
+  );
+});
+
+const StyledBackdrop = styled(Backdrop)`
+  align-items: flex-start;
+  justify-content: flex-end;
+  ${({ theme }) => theme.insetSafeArea.top('top', `${headerHeight}px`, '+')}
+`;
+
+const StyledRoot = styled.div`
+  background-color: ${({ theme }) => theme.color.palette.main.background};
+  width: 200px;
+  height: 100%;
+  & > nav {
+    & > ul {
+      & > li {
+        & > a {
+          display: inline-block;
+          width: 100%;
+          & button {
+            width: 100%;
+            background-color: ${({ theme }) => theme.color.palette.main.background};
+            text-align: left;
+            font-size: ${({ theme }) => theme.font.size.large}px;
+            font-weight: ${({ theme }) => theme.font.weight.bold};
+            padding: ${({ theme }) => theme.space.large}px;
+            padding-left: ${({ theme }) => theme.space.large * 2}px;
+          }
+        }
+      }
+    }
+  }
+`;

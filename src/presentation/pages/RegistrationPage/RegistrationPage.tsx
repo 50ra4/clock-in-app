e67975ac21e8e4e86a882e1db0beceb07f0bba86
50ra4 <50ra4.dev.dev@gmail.com>
2021-08-14 23:10:08 +0900

    add: RegistrationPage

--- src/presentation/pages/RegistrationPage/RegistrationPage.tsx
+++ src/presentation/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,16 +1,99 @@
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+
 import { WithHeaderLayout } from 'presentation/layouts/WithHeaderLayout/WithHeaderLayout';
+import { Card } from 'presentation/components/surfaces/Card/Card';
+import { TextForm } from 'presentation/components/forms/TextForm/TextForm';
+import { PasswordForm } from 'presentation/components/forms/PasswordForm/PasswordForm';
+import { Button } from 'presentation/components/inputs/Button/Button';
+
+import { signUpUser } from 'thunks/authentication';
+import { PAGE_PATH } from 'constants/path';
+import { AppState } from 'store/root';
+import { LoadingGuard } from 'presentation/components/feedback/LoadingGuard/LoadingGuard';
 
 const RegistrationPage = () => {
+  const isLoading = useSelector((state: AppState) => state.authentication.fetchStatus === 'FETCHING');
+
   return (
     <WithHeaderLayout>
+      <LoadingGuard open={isLoading} />
       <StyledRoot>
-        <h2>RegistrationPage</h2>
+        <StyledCard title="アカウント登録">
+          <RegistrationPageForm />
+        </StyledCard>
       </StyledRoot>
     </WithHeaderLayout>
   );
 };
 
-const StyledRoot = styled.div``;
+const StyledRoot = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-top: 100px;
+`;
+const StyledCard = styled(Card)`
+  width: 100%;
+  max-width: 480px;
+`;
+
+type Dispatch = Parameters<ReturnType<typeof signUpUser>>[0];
+
+const RegistrationPageForm = React.memo(function RegistrationPageForm() {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch<Dispatch>();
+  const signUp = useCallback(async () => {
+    const result = await dispatch(signUpUser(email, password));
+    if (!result) {
+      // TODO: error handling
+      window.alert('error');
+      return;
+    }
+    history.replace(PAGE_PATH.top);
+  }, [dispatch, email, history, password]);
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    signUp();
+  };
+
+  return (
+    <StyledForm onSubmit={handleOnSubmit}>
+      <StyledTextForm
+        id="email"
+        name="email"
+        label="メールアドレス"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+        onClear={() => setEmail('')}
+      />
+      <StyledPasswordForm
+        id="password"
+        name="password"
+        label="パスワード"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+      <Button type="submit" color="secondary" fullWidth={true}>
+        アカウント登録
+      </Button>
+    </StyledForm>
+  );
+});
+
+const StyledForm = styled.form``;
+const StyledTextForm = styled(TextForm)`
+  margin-top: 10px;
+  margin-bottom: 20px;
+`;
+const StyledPasswordForm = styled(PasswordForm)`
+  margin-bottom: 20px;
+`;
 
 export default RegistrationPage;

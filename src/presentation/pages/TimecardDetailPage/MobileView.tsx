efe60642a446a05a490a145d16ae7ed6572b365f
50ra4 <50ra4.dev.dev@gmail.com>
2021-09-08 21:03:39 +0900

    add: mockData for TimecardDetailPage

--- src/presentation/pages/TimecardDetailPage/MobileView.tsx
+++ src/presentation/pages/TimecardDetailPage/MobileView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import styled from 'styled-components';
 
 import { DATE_FORMAT } from 'constants/dateFormat';
@@ -6,6 +6,7 @@ import { isValidDateString, getThisMonthDateString } from 'utils/dateUtil';
 import { ResponsiveLayout } from 'presentation/layouts/ResponsiveLayout/ResponsiveLayout';
 import { useSyncStateWithURLQueryString } from 'hooks/useSyncStateWithURLQueryString';
 import { MonthSelector } from './components/MonthSelector';
+import { mockTimeCards } from './mockData';
 
 const THIS_MONTH_DATE_STRING = getThisMonthDateString();
 
@@ -40,10 +41,23 @@ export const MobileView = React.memo(function MobileView() {
     [setQuery],
   );
 
+  const monthlyTimeCard = useMemo(() => {
+    // FIXME: replace data
+    const mockData = mockTimeCards.find(({ month }) => month === selectedMonth) ?? {
+      month: selectedMonth,
+      dailyRecords: [],
+    };
+    // eslint-disable-next-line no-console
+    console.log(mockTimeCards, mockData);
+    return mockData;
+  }, [selectedMonth]);
+
   return (
     <StyledRoot>
       <h2>Mobile view</h2>
       <MonthSelector selectedMonth={selectedMonth} onChangeMonth={updateSelectedMonth} />
+      {monthlyTimeCard.month}
+      {monthlyTimeCard.dailyRecords.map(({ date }) => date).join('\n')}
     </StyledRoot>
   );
 });

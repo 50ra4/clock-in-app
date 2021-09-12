c9f88ef1b3052fc92e6013124ef24bce3116e991
50ra4 <50ra4.dev.dev@gmail.com>
2021-09-12 16:21:19 +0900

    fix: stories mock data

--- src/presentation/pages/TimecardDetailPage/components/MonthlyTimeCardTable.stories.tsx
+++ src/presentation/pages/TimecardDetailPage/components/MonthlyTimeCardTable.stories.tsx
@@ -1,7 +1,5 @@
 import { MonthlyTimeCardTable } from './MonthlyTimeCardTable';
 import { createStoryMeta, createStoryTemplate } from 'utils/storybookUtils';
-import { getThisMonthDateString } from 'utils/dateUtil';
-import { mockTimeCards } from '../mockData';
 
 export default createStoryMeta(MonthlyTimeCardTable, {
   title: 'pages/TimecardDetailPage/MonthlyTimeCardTable',
@@ -9,14 +7,150 @@ export default createStoryMeta(MonthlyTimeCardTable, {
 
 const Template = createStoryTemplate(MonthlyTimeCardTable);
 
-const THIS_MONTH_DATE_STRING = getThisMonthDateString();
-const thisMonthData = mockTimeCards.find(({ month }) => month === THIS_MONTH_DATE_STRING) ?? {
-  month: THIS_MONTH_DATE_STRING,
-  dailyRecords: [],
+const mockData = {
+  month: '2021-07',
+  dailyRecords: [
+    {
+      start: {
+        hour: 10,
+        minute: 57,
+      },
+      date: '2021-07-01',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks: 'ああああああああああああああああああああああああああ',
+    },
+    {
+      start: {
+        hour: 12,
+        minute: 9,
+      },
+      end: {
+        hour: 15,
+        minute: 42,
+      },
+      date: '2021-07-02',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks:
+        'ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      start: {
+        hour: 13,
+        minute: 19,
+      },
+      end: {
+        hour: 19,
+        minute: 53,
+      },
+      date: '2021-07-03',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks: 'ああああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      start: {
+        hour: 11,
+        minute: 17,
+      },
+      end: {
+        hour: 20,
+        minute: 51,
+      },
+      date: '2021-07-04',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks:
+        'ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      start: {
+        hour: 13,
+        minute: 5,
+      },
+      date: '2021-07-05',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks: 'あああああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      start: {
+        hour: 12,
+        minute: 25,
+      },
+      end: {
+        hour: 20,
+        minute: 54,
+      },
+      date: '2021-07-06',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks: 'あああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      start: {
+        hour: 14,
+        minute: 15,
+      },
+      end: {
+        hour: 20,
+        minute: 10,
+      },
+      date: '2021-07-07',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks: 'あああああああああああああああああああああああああああああ',
+    },
+    {
+      start: {
+        hour: 14,
+        minute: 57,
+      },
+      end: {
+        hour: 18,
+        minute: 37,
+      },
+      date: '2021-07-08',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks: 'ああああああああああ',
+    },
+    {
+      start: {
+        hour: 11,
+        minute: 27,
+      },
+      end: {
+        hour: 19,
+        minute: 7,
+      },
+      date: '2021-07-09',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks:
+        'あああああああああああああああああああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      start: {
+        hour: 9,
+        minute: 4,
+      },
+      end: {
+        hour: 13,
+        minute: 9,
+      },
+      date: '2021-07-10',
+      inHouseWorks: [],
+      restTimes: [],
+      remarks:
+        'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ',
+    },
+  ],
 };
 
 export const Docs = Template;
 Docs.args = {
-  month: thisMonthData.month,
-  dailyRecords: thisMonthData.dailyRecords,
+  month: mockData.month,
+  dailyRecords: mockData.dailyRecords,
 };

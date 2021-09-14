d0c0d5bbb142618bc709abc3c5e468e7ac509322
50ra4 <50ra4.dev.dev@gmail.com>
2021-09-14 09:53:01 +0900

    apply: TimeRangeGroupForm to InputRecordForm

--- src/presentation/pages/TimecardDetailPage/components/InputRecordForm.tsx
+++ src/presentation/pages/TimecardDetailPage/components/InputRecordForm.tsx
@@ -1,8 +1,9 @@
 import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import { TimeForm } from 'presentation/components/forms/TimeForm/TimeForm';
-import { DailyTimeRecord, Time } from 'types';
+import { DailyTimeRecord, Time, Range } from 'types';
 import { TextAreaForm } from 'presentation/components/forms/TextAreaForm/TextAreaForm';
+import { TimeRangeGroupForm } from 'presentation/components/forms/TimeRangeGroupForm/TimeRangeGroupForm';
 
 type Props = {
   className?: string;
@@ -10,7 +11,11 @@ type Props = {
 };
 
 export const InputRecordForm = React.memo(function InputRecordForm({ className, dailyTimeRecord }: Props) {
-  const [state, setState] = useState({ ...dailyTimeRecord });
+  const [state, setState] = useState({
+    ...dailyTimeRecord,
+    restTimes:
+      dailyTimeRecord.restTimes.length > 0 ? dailyTimeRecord.restTimes : [{ start: undefined, end: undefined }],
+  });
 
   const onChangeClockInTime = useCallback((time: Time) => {
     setState((prev) => ({ ...prev, start: time }));
@@ -21,30 +26,42 @@ export const InputRecordForm = React.memo(function InputRecordForm({ className, 
   const onChangeRemarks = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setState((prev) => ({ ...prev, remarks: e.target?.value ?? '' }));
   }, []);
+  const onChangeRestTimes = useCallback((timeRange: Range<Time>[]) => {
+    setState((prev) => ({ ...prev, restTimes: timeRange }));
+  }, []);
 
   return (
     <StyledRoot className={className}>
       <TimeForm
+        type="text"
         id="clock-in-time"
         name="clock-in-time"
         value={state.start}
         label="出社"
-        type="input"
         required={false}
         inline={true}
         onBlur={onChangeClockInTime}
         onChange={onChangeClockInTime}
       />
       <TimeForm
+        type="text"
         id="clock-out-time"
         name="clock-out-time"
         value={state.end}
         label="退社"
-        type="input"
         required={false}
         inline={true}
         onBlur={onChangeClockOutTime}
         onChange={onChangeClockOutTime}
+      />
+      <TimeRangeGroupForm
+        type="text"
+        id="rest-time"
+        name="rest-time"
+        label="休憩時間"
+        value={state.restTimes}
+        inline={true}
+        onChange={onChangeRestTimes}
       />
       <TextAreaForm
         id="remarks"
@@ -60,6 +77,10 @@ export const InputRecordForm = React.memo(function InputRecordForm({ className, 
 });
 
 const StyledRoot = styled.div`
+  overflow-y: scroll;
+  max-height: calc(100vh - 160px);
+  ${({ theme }) => theme.scrollBar.hidden()}
+
   & > * {
     padding: ${({ theme }) => theme.space.middle}px;
   }

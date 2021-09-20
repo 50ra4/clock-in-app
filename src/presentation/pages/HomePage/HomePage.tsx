822cf1f6f133dc6c4ba502deedfe294ae4a0a5f6
50ra4 <50ra4.dev.dev@gmail.com>
2021-09-20 16:23:28 +0900

    add: useLoginRedirection to HomePage

--- src/presentation/pages/HomePage/HomePage.tsx
+++ src/presentation/pages/HomePage/HomePage.tsx
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 import { WithHeaderLayout } from 'presentation/layouts/WithHeaderLayout/WithHeaderLayout';
+import { useLoginRedirection } from 'hooks/useAuthentication';
 
 const HomePage = () => {
+  useLoginRedirection();
+
   return (
     <WithHeaderLayout>
       <HomePageContents />

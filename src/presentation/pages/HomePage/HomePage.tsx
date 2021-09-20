5282419cca550929b8ae7374b6e67d17bb030d22
50ra4 <50ra4.dev.dev@gmail.com>
2021-09-20 17:59:01 +0900

    add: timecard link to HomePage

--- src/presentation/pages/HomePage/HomePage.tsx
+++ src/presentation/pages/HomePage/HomePage.tsx
@@ -1,25 +1,41 @@
+import React from 'react';
 import styled from 'styled-components';
+
 import { WithHeaderLayout } from 'presentation/layouts/WithHeaderLayout/WithHeaderLayout';
-import { useLoginRedirection } from 'hooks/useAuthentication';
+import { Link } from 'presentation/components/navigation/Link/Link';
+import { useAuthentication, useLoginRedirection } from 'hooks/useAuthentication';
+import { replacePathParams } from 'utils/pathUtil';
+import { PAGE_PATH } from 'constants/path';
 
 const HomePage = () => {
   useLoginRedirection();
+  const { loggedInUid: uid } = useAuthentication();
 
   return (
     <WithHeaderLayout>
-      <HomePageContents />
+      <HomePageContents toTimeCard={replacePathParams(PAGE_PATH.timecardDetail, { uid })} />
     </WithHeaderLayout>
   );
 };
 
-const HomePageContents = () => {
+const HomePageContents = React.memo(function HomePageContents({ toTimeCard }: { toTimeCard: string }) {
   return (
     <StyledRoot>
       <h2>HomePage</h2>
+      <StyledDescription>
+        <Link to={toTimeCard}>自分のタイムカードを確認する</Link>
+      </StyledDescription>
     </StyledRoot>
   );
-};
+});
 
 const StyledRoot = styled.div``;
+const StyledDescription = styled.p`
+  max-width: 300px;
+  border: 1px solid ${({ theme }) => theme.color.border.dark};
+  border-radius: 5px;
+  padding: 15px 20px;
+  margin: ${({ theme }) => theme.space.middle}px;
+`;
 
 export default HomePage;

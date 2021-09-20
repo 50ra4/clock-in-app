f16f357595f18fb7bc42b94e89b2c8f2c75b5735
50ra4 <50ra4.dev.dev@gmail.com>
2021-09-20 16:52:24 +0900

    add: Link to Login and Registration pages

--- src/presentation/pages/TopPage/TopPage.tsx
+++ src/presentation/pages/TopPage/TopPage.tsx
@@ -1,22 +1,53 @@
 import styled from 'styled-components';
 import { WithHeaderLayout } from 'presentation/layouts/WithHeaderLayout/WithHeaderLayout';
-
-const text =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+import { PAGE_PATH } from 'constants/path';
+import { Link } from 'presentation/components/navigation/Link/Link';
+import { ResponsiveLayout } from 'presentation/layouts/ResponsiveLayout/ResponsiveLayout';
 
 const TopPage = () => {
   return (
     <WithHeaderLayout>
-      <StyledRoot>
-        <h2>TopPage</h2>
-        <div>
-          <p>{text}</p>
-        </div>
+      <StyledRoot placement="left">
+        <StyledPR>
+          <strong>"Clock in"</strong>はブラウザ上で勤怠管理ができるWebアプリケーションです。
+        </StyledPR>
+        <StyledDescription>
+          初めての方は<Link to={PAGE_PATH.registration}>新規アカウント登録</Link>
+        </StyledDescription>
+        <StyledDescription>
+          既にアカウントをお持ちの方は<Link to={PAGE_PATH.login}>ログイン</Link>
+        </StyledDescription>
       </StyledRoot>
     </WithHeaderLayout>
   );
 };
 
-const StyledRoot = styled.div``;
+const StyledRoot = styled(ResponsiveLayout)`
+  ${({ theme }) =>
+    theme.insetSafeArea.topBottom('min-height', `calc(100vh - ${theme.height.header - theme.space.large * 2}px)`, '-')};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & > div {
+  }
+`;
+const StyledPR = styled.h2`
+  font-size: ${({ theme }) => theme.font.size.extraLarge}px;
+  color: ${({ theme }) => theme.color.font.black};
+  & > strong {
+    font-weight: ${({ theme }) => theme.font.weight.bold};
+  }
+  width: 100%;
+  padding: 15px 20px;
+  margin: ${({ theme }) => theme.space.middle}px;
+`;
+const StyledDescription = styled.p`
+  width: 100%;
+  border: 1px solid ${({ theme }) => theme.color.border.dark};
+  border-radius: 5px;
+  padding: 15px 20px;
+  margin: ${({ theme }) => theme.space.middle}px;
+`;
 
 export default TopPage;

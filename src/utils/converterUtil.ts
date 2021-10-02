318f80dbfc0cd7c4cf19fa58026becf7f9dc0757
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-02 17:03:30 +0900

    add: omitUndefinedProps util

---
+++ src/utils/converterUtil.ts
@@ -0,0 +1,2 @@
+export const omitUndefinedProps = <T extends Record<string, unknown>>(obj: T) =>
+  Object.fromEntries(Object.entries(obj).filter(([_, value]) => typeof value !== 'undefined')) as T;

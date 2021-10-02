97171ce85ed97d8f4325e68f295513472c0cb891
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-02 17:01:39 +0900

    fix: replacePathParams path type

--- src/utils/pathUtil.ts
+++ src/utils/pathUtil.ts
@@ -1,4 +1,7 @@
-export const replacePathParams = <T extends Record<string, string>>(path: string, pathParams: T): string =>
+export const replacePathParams = <Path extends string, T extends Record<string, string>>(
+  path: Path,
+  pathParams: T,
+): string =>
   Object.entries(pathParams).reduce((pre, [key, value]) => {
     const regex = new RegExp(`/:${key}`);
     return pre.replace(regex, `/${value}`);

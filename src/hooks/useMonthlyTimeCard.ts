4a7ffb12ae183112c92f63d3b6da599d0187027e
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-06 23:45:32 +0900

    add: read daily inhouseWork data

--- src/hooks/useMonthlyTimeCard.ts
+++ src/hooks/useMonthlyTimeCard.ts
@@ -2,8 +2,9 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 import { firestore } from 'services/firebase';
 import { replacePathParams } from 'utils/pathUtil';
 import { DAILY_RECORDS_COLLECTION_PATH } from 'constants/firestore';
-import { DailyTimeRecord } from 'types';
-import { saveDailyTimeRecord } from 'services/dailyTimeRecord';
+import { DailyTimeRecord, InHouseWork } from 'types';
+import { getInHouseWorks, queryToDailyTimeRecord, saveDailyTimeRecord } from 'services/dailyTimeRecord';
+import { usePreviousRef } from './usePreviousRef';
 
 type Props = {
   uid: string;
@@ -11,7 +12,8 @@ type Props = {
 };
 
 export const useMonthlyTimeCard = ({ uid, month }: Props) => {
-  const [timecard, setTimeCard] = useState(new Map<string, DailyTimeRecord>());
+  const [dailyTimeRecords, setDailyTimeRecords] = useState(new Map<string, DailyTimeRecord>());
+  const [inHouseWorks, setInHouseWorks] = useState(new Map<string, InHouseWork[]>());
 
   const createDailyTimeRecord = useCallback(
     (data: DailyTimeRecord) => {
@@ -21,26 +23,30 @@ export const useMonthlyTimeCard = ({ uid, month }: Props) => {
     [uid],
   );
 
+  const previousDailyTimeRecords = usePreviousRef(dailyTimeRecords);
+
   useEffect(() => {
-    const path = replacePathParams(DAILY_RECORDS_COLLECTION_PATH, { uid, month });
-    const unsubscribe = firestore.collection(path).onSnapshot(
+    if (!uid || !month) {
+      return;
+    }
+
+    const collectionPath = replacePathParams(DAILY_RECORDS_COLLECTION_PATH, { uid, month });
+    const unsubscribe = firestore.collection(collectionPath).onSnapshot(
       (snapshot) => {
-        setTimeCard((prev) => {
-          const updated = new Map(prev);
-          snapshot.forEach((doc) => {
-            const date = doc.get('date');
-            const start = doc.get('start');
-            const end = doc.get('end');
-            const remarks = doc.get('remarks');
-            updated.set(date, { date, start, end, remarks, restTimes: [], inHouseWorks: [] });
-          });
-          return updated;
+        const updated = new Map(previousDailyTimeRecords.current);
+        snapshot.docChanges().forEach(({ type, doc }) => {
+          if (type === 'removed') {
+            updated.delete(doc.id);
+            return;
+          }
+          updated.set(doc.id, queryToDailyTimeRecord(doc));
         });
+        setDailyTimeRecords(updated);
       },
       (error) => {
         // TODO: error handling
         // eslint-disable-next-line no-console
-        console.error(path, error);
+        console.error(collectionPath, error);
       },
       () => {
         // TODO: ???
@@ -51,13 +57,37 @@ export const useMonthlyTimeCard = ({ uid, month }: Props) => {
     return () => {
       unsubscribe();
     };
-  }, [month, uid]);
+  }, [month, previousDailyTimeRecords, uid]);
+
+  useEffect(() => {
+    if (!uid || !month) {
+      return;
+    }
+
+    const days = Array.from(dailyTimeRecords.keys());
+    const getInHouseWorksOfMonth = days.map(async (date) => ({
+      date,
+      inHouseWorks: await getInHouseWorks(uid, date),
+    }));
+
+    Promise.all(getInHouseWorksOfMonth).then((res) => {
+      const updated = new Map<string, InHouseWork[]>();
+      res.forEach(({ date, inHouseWorks }) => updated.set(date, inHouseWorks));
+      setInHouseWorks(updated);
+    });
+  }, [dailyTimeRecords, month, uid]);
 
   const data = useMemo(() => {
     // FIXME: modify the dailyRecords type definition
-    const dailyRecords = Array.from(timecard.values());
+    const dailyRecords = Array.from(dailyTimeRecords.values()).map((record) => ({
+      ...record,
+      inHouseWorks: inHouseWorks.get(record.date) ?? [],
+    }));
+    // TODO: remove
+    // eslint-disable-next-line no-console
+    console.log(dailyRecords);
     return { month, dailyRecords };
-  }, [month, timecard]);
+  }, [dailyTimeRecords, inHouseWorks, month]);
 
   return {
     data,

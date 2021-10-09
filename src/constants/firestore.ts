fcbf1e898c7c2352012244c2b6572edd6a35ccd2
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-09 17:05:53 +0900

    add: RestTime to dailyTimeRecord service

--- src/constants/firestore.ts
+++ src/constants/firestore.ts
@@ -10,3 +10,5 @@ export const MONTHLY_TIMECARD_DOCUMENT_PATH = `${USER_COLLECTION_PATH}/:uid/time
 export const DAILY_RECORDS_COLLECTION_PATH = `${MONTHLY_TIMECARD_DOCUMENT_PATH}/days` as const;
 
 export const DAILY_IN_HOUSE_WORK_COLLECTION_PATH = `${DAILY_RECORDS_COLLECTION_PATH}/:day/in-house-works` as const;
+
+export const DAILY_REST_TIME_COLLECTION_PATH = `${DAILY_RECORDS_COLLECTION_PATH}/:day/rest-times` as const;

ad77749379ae6d733780223f4db4e31330697500
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-09 18:15:27 +0900

    add: crashed prop to AppError

--- src/models/AppError.ts
+++ src/models/AppError.ts
@@ -4,12 +4,17 @@ export class AppError extends Error {
   public readonly name = 'AppError';
   public readonly code: ErrorCode;
   public readonly message: string;
+  public readonly crashed: boolean;
   public readonly stack?: string;
 
-  constructor(code: ErrorCode, { message, stack }: { message: string; stack?: string }) {
+  constructor(
+    code: ErrorCode,
+    { message, stack, crashed = false }: { message: string; stack?: string; crashed?: boolean },
+  ) {
     super();
     this.code = code;
     this.message = message;
+    this.crashed = crashed;
     this.stack = stack;
   }
 }
--- src/utils/errorUtil.ts
+++ src/utils/errorUtil.ts
@@ -7,7 +7,8 @@ export const errorToHeadingWithMessage = <E extends Error>(
 ): ErrorHeadingWithMessage & { crashed?: boolean } => {
   // TODO: 中身を実装する
   if (error instanceof AppError) {
-    return { ...ERROR_HEADING_WITH_MESSAGE[error.code], crashed: true };
+    const headingWithMessage = ERROR_HEADING_WITH_MESSAGE[error.code];
+    return { ...headingWithMessage, crashed: error.crashed };
   }
 
   return { ...ERROR_HEADING_WITH_MESSAGE.UNKNOWN, crashed: true };

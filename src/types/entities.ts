f0ecd369b7ecddfe7b7ba2ff315f9d70f8d449ab
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-19 00:03:19 +0900

    fix: splitting types file

---
+++ src/types/entities.ts
@@ -0,0 +1,43 @@
+import { Time, LoginStatus } from './utils';
+
+export type RestTime = {
+  id: string | undefined;
+  start?: Time;
+  end?: Time;
+};
+
+export type InHouseWork = {
+  id: string | undefined;
+  start?: Time;
+  end?: Time;
+  remarks?: string;
+};
+
+// FIXME: type name
+export type DailyTimeRecord = {
+  /** NOTE: date instead of id  */
+  id?: string;
+  date: string;
+  start?: Time;
+  end?: Time;
+  inHouseWorks: InHouseWork[];
+  restTimes: RestTime[];
+  remarks: string;
+  // TODO: holiday ...etc
+};
+
+export type User = {
+  firstName: string;
+  lastName: string;
+};
+export type Profile = User & {
+  uid?: string;
+  email: string | null;
+  photoURL: string | null;
+  displayName: string | null;
+};
+export type Authentication = {
+  uid: string;
+  loginStatus: LoginStatus;
+  emailVerified: boolean;
+};

8c0db4cf5f9e97eac893cdd969a10d09b880d082
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-23 17:00:57 +0900

    add: createTestString to testUtil

--- src/utils/testUtil.ts
+++ src/utils/testUtil.ts
@@ -1,5 +1,10 @@
 import { ValidationOption, Validator } from './validationUtil';
 
+export const createTestString = (length: number) =>
+  Array.from({ length })
+    .map(() => 'a')
+    .join('');
+
 export const createExpectForValidator = <T>(option: ValidationOption, validator: Validator<T>) => {
   return (value: T | undefined) => {
     const result = validator(option)(value);

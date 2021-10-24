e53a2c9f6a6d9e943c988e78b4a7fd905e0a1e85
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-24 17:25:36 +0900

    add: Nullable to types

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,5 +1,7 @@
 import { FETCH_STATUS_ENUM, LOGIN_STATUS_ENUM } from 'constants/enum';
 
+export type Nullable<T> = T | undefined | null;
+
 export type EnumValue<O extends { [K: string]: string }> = O[keyof O];
 export type FetchStatus = EnumValue<typeof FETCH_STATUS_ENUM>;
 export type LoginStatus = EnumValue<typeof LOGIN_STATUS_ENUM>;

5926e42eefc5c0e183b9747d56def18224e78933
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-24 23:35:17 +0900

    update: VALIDATION_ERROR_MESSAGE

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -35,7 +35,7 @@ export const ERROR_HEADING_WITH_MESSAGE: Readonly<Record<ErrorCode, ErrorHeading
 
 export const VALIDATION_ERROR_MESSAGE = {
   isEmpty: '%displayName%を入力してください',
-  typeIsInvalid: '%displayName%の値が不正です',
+  typeIsInvalid: '%displayName%の型が不正です',
   hourIsEmpty: '時刻を入力してください',
   hourIsOutOfRange: '時刻を0から23の範囲で入力してください',
   minuteIsEmpty: '分を入力してください',

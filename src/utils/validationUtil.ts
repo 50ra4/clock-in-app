a368d129859c925d41c2399ed8a93ccc19e74410
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-24 23:42:35 +0900

    add: displayName to ValidationFactory

--- src/utils/validationUtil.ts
+++ src/utils/validationUtil.ts
@@ -2,6 +2,7 @@ import { Either, right, left } from 'fp-ts/Either';
 import { VALIDATION_ERROR_MESSAGE } from 'constants/error';
 import { EnumValue, Nullable } from 'types';
 import { isNullable } from './typeGuard';
+import { replaceMessage } from './messageUtil';
 
 type Validate<T> = (value: T) => boolean;
 export type ValidationErrorMessage = EnumValue<typeof VALIDATION_ERROR_MESSAGE>;
@@ -10,6 +11,7 @@ export class ValidationError implements TypeError {
   public readonly name = 'ValidationError';
 
   constructor(
+    // TODO: add type name
     public readonly value: unknown, //
     public readonly message: string,
   ) {}
@@ -22,9 +24,13 @@ export type ValidationOption = {
 // TODO: remove (old type)
 export type Validator<T> = (option: ValidationOption) => (value: T | undefined) => ValidationError | false;
 
+export const failed = (value: unknown, message: string): Either<ValidationError, true> =>
+  left(new ValidationError(value, message));
+
 export class ValidationFactory<Value, Message extends string = string> {
   constructor(
     public readonly name: string,
+    private readonly displayName: string,
     /** type guard */
     private readonly is: (x: unknown) => x is Value,
     private readonly isEmpty: (value: Nullable<Value>) => boolean = isNullable,
@@ -41,23 +47,27 @@ export class ValidationFactory<Value, Message extends string = string> {
   }
 
   public create({ required }: ValidationOption) {
+    const { is, isEmpty, displayName, validators } = this;
+
     // eslint-disable-next-line complexity
-    return (value: Nullable<Value>): Either<ValidationError, false> => {
-      if (!this.is(value)) {
-        return left(new ValidationError(value, VALIDATION_ERROR_MESSAGE.typeIsInvalid));
+    return (value: Nullable<Value>): Either<ValidationError, true> => {
+      if (!is(value)) {
+        const message = replaceMessage(VALIDATION_ERROR_MESSAGE.typeIsInvalid, { displayName });
+        return failed(value, message);
       }
-      if (this.isEmpty(value)) {
+      if (isEmpty(value)) {
         if (!required) {
-          return right(false);
+          return right(true);
         }
-        return left(new ValidationError(value, VALIDATION_ERROR_MESSAGE.isEmpty));
+        const message = replaceMessage(VALIDATION_ERROR_MESSAGE.isEmpty, { displayName });
+        return failed(value, message);
       }
-      for (const { isValid, message } of this.validators) {
+      for (const { isValid, message } of validators) {
         if (!isValid(value)) {
-          return left(new ValidationError(value, message));
+          return failed(value, message);
         }
       }
-      return right<ValidationError, false>(false);
+      return right(true);
     };
   }
 }

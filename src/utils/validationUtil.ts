e3fd0b45b35717a8559bc0d2324ba949deeeb591
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-24 18:03:58 +0900

    add: ValidationFactory to validationUtil

--- src/utils/validationUtil.ts
+++ src/utils/validationUtil.ts
@@ -1,3 +1,11 @@
+import { Either, right, left, isLeft } from 'fp-ts/Either';
+import { VALIDATION_ERROR_MESSAGE } from 'constants/error';
+import { EnumValue, Nullable } from 'types';
+import { isNullable } from './typeGuard';
+
+type Validate<T> = (value: T) => boolean;
+export type ValidationErrorMessage = EnumValue<typeof VALIDATION_ERROR_MESSAGE>;
+
 export class ValidationError implements TypeError {
   public readonly name = 'ValidationError';
 
@@ -11,4 +19,48 @@ export type ValidationOption = {
   required?: boolean;
 };
 
+// TODO: remove (old type)
 export type Validator<T> = (option: ValidationOption) => (value: T | undefined) => ValidationError | false;
+
+export class ValidationFactory<Value, Message extends string = string> {
+  constructor(
+    public readonly name: string,
+    /** type guard */
+    private readonly is: (x: unknown) => x is Value,
+    private readonly isEmpty: (value: Nullable<Value>) => boolean = isNullable,
+  ) {}
+
+  private readonly validators: {
+    isValid: Validate<Value>;
+    message: Message;
+  }[] = [];
+
+  public add(isValid: Validate<Value>, message: Message) {
+    this.validators.push({ isValid, message });
+    return this;
+  }
+
+  public create({ required }: ValidationOption) {
+    return (value: Nullable<Value>): Either<ValidationError, false> => {
+      if (!this.is(value)) {
+        return left(new ValidationError(value, VALIDATION_ERROR_MESSAGE.typeIsInvalid));
+      }
+      if (this.isEmpty(value)) {
+        if (!required) {
+          return right(false);
+        }
+        return left(new ValidationError(value, VALIDATION_ERROR_MESSAGE.isEmpty));
+      }
+      return this.validators.reduce((acc, { isValid, message }) => {
+        if (isLeft(acc)) {
+          // If already in error
+          return acc;
+        }
+        if (isValid(value)) {
+          return acc;
+        }
+        return left(new ValidationError(value, message));
+      }, right<ValidationError, false>(false));
+    };
+  }
+}

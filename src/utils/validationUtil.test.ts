0cf9586a33bec1aece70f622cddebf4dc14acc85
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-24 23:42:57 +0900

    update: ValidationFactory test case

--- src/utils/validationUtil.test.ts
+++ src/utils/validationUtil.test.ts
@@ -1,9 +1,10 @@
 import { VALIDATION_ERROR_MESSAGE } from 'constants/error';
 import * as either from 'fp-ts/lib/Either';
 import { EnumValue, Nullable } from 'types';
+import { replaceMessage } from './messageUtil';
 import { createTestString } from './testUtil';
 import { isObject, hasPropertiesInObject, isNonNullable } from './typeGuard';
-import { ValidationError, ValidationErrorMessage, ValidationFactory } from './validationUtil';
+import { ValidationErrorMessage, ValidationFactory, failed } from './validationUtil';
 
 type User = {
   id: string;
@@ -19,39 +20,49 @@ const MESSAGE = {
   isEmptyUserName: 'ユーザー名を入力してください',
   isOverLength: 'ユーザー名を50文字以内で入力してください',
 } as const;
+const typeName = 'User';
+const displayName = 'ユーザー';
 
 type Message = ValidationErrorMessage | EnumValue<typeof MESSAGE>;
 
 describe('validationUtil', () => {
   describe('ValidationFactory', () => {
-    const factory = new ValidationFactory<User, Message>('user', isUser, isEmptyUser);
+    const factory = new ValidationFactory<User, Message>(typeName, displayName, isUser, isEmptyUser);
     factory
       .add((user) => !!user.name, MESSAGE.isEmptyUserName)
       .add((user) => user.name.length < 51, MESSAGE.isOverLength);
 
+    it('should return true', () => {
+      const user1 = { id: '1', name: 'うずまき', age: 12 };
+      const validate1 = factory.create({ required: true });
+      expect(validate1(user1)).toEqual(either.right(true));
+      const validate2 = factory.create({ required: false });
+      expect(validate2(user1)).toEqual(either.right(true));
+    });
+
     it('should return type guard message', () => {
       const validate = factory.create({ required: true });
       const user1 = { id: '', name: '' } as User;
-      expect(validate(user1)).toEqual(either.left(new ValidationError(user1, MESSAGE.isInvalidType)));
+      expect(validate(user1)).toEqual(failed(user1, replaceMessage(MESSAGE.isInvalidType, { displayName })));
     });
 
     it('should return empty error message', () => {
       const validate = factory.create({ required: true });
       const user1 = null;
-      expect(validate(user1)).toEqual(either.left(new ValidationError(user1, MESSAGE.isEmptyUser)));
+      expect(validate(user1)).toEqual(failed(user1, replaceMessage(MESSAGE.isEmptyUser, { displayName })));
       const user2 = undefined;
-      expect(validate(user2)).toEqual(either.left(new ValidationError(user2, MESSAGE.isEmptyUser)));
+      expect(validate(user2)).toEqual(failed(user2, replaceMessage(MESSAGE.isEmptyUser, { displayName })));
       const user3 = { id: '', name: '', age: 12 };
-      expect(validate(user3)).toEqual(either.left(new ValidationError(user3, MESSAGE.isEmptyUser)));
+      expect(validate(user3)).toEqual(failed(user3, replaceMessage(MESSAGE.isEmptyUser, { displayName })));
     });
 
     it('should return invalid error message', () => {
       const validate = factory.create({ required: false });
       const user1 = { id: '1', name: '', age: 12 };
-      expect(validate(user1)).toEqual(either.left(new ValidationError(user1, MESSAGE.isEmptyUserName)));
+      expect(validate(user1)).toEqual(failed(user1, MESSAGE.isEmptyUserName));
 
       const user2 = { id: '1', name: createTestString(51), age: 12 };
-      expect(validate(user2)).toEqual(either.left(new ValidationError(user2, MESSAGE.isOverLength)));
+      expect(validate(user2)).toEqual(failed(user2, MESSAGE.isOverLength));
     });
   });
 });

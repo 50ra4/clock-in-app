0286f4dbc63908f54aaa843a483128dddb0cd7bb
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-25 20:22:28 +0900

    refactor: validationUtil type definitions

--- src/utils/validationUtil.ts
+++ src/utils/validationUtil.ts
@@ -4,7 +4,6 @@ import { EnumValue, Nullable, NullOrUndefined } from 'types';
 import { isNullable } from './typeGuard';
 import { replaceMessage } from './messageUtil';
 
-type Validate<T> = (value: T) => boolean;
 export type ValidationErrorMessage = EnumValue<typeof VALIDATION_ERROR_MESSAGE>;
 
 export class ValidationError implements TypeError {
@@ -20,9 +19,17 @@ export class ValidationError implements TypeError {
 export type ValidationOption = {
   required?: boolean;
 };
+type Validate<Input> = (value: Input, option: ValidationOption) => boolean;
+type ValidateWithMessage<Input, Message extends string = string> = {
+  validate: Validate<Input>;
+  message: Message;
+};
 
-// TODO: remove (old type)
-export type Validator<T> = (option: ValidationOption) => (value: T | undefined) => ValidationError | false;
+/**
+ * TODO: remove
+ * @deprecated old type
+ */
+export type Validator<Input> = (option: ValidationOption) => (value: Input | undefined) => ValidationError | false;
 
 export const failed = (value: unknown, message: string): Either<ValidationError, true> =>
   left(new ValidationError(value, message));
@@ -36,37 +43,34 @@ const createIsEmpty =
   (x): x is NullOrUndefined =>
     isNullable<T>(x);
 
-export class ValidationFactory<Value, Message extends string = string> {
+export class ValidationFactory<Input, Message extends string = string> {
   constructor(
     private readonly name: string,
     private readonly displayName: string,
-    private readonly isEmpty: ValidateIsEmpty<Value> = createIsEmpty<Value>(),
+    private readonly isEmpty: ValidateIsEmpty<Input> = createIsEmpty<Input>(),
   ) {}
 
-  private readonly validators: {
-    isValid: Validate<Value>;
-    message: Message;
-  }[] = [];
+  private readonly validators: ValidateWithMessage<Input, Message>[] = [];
 
-  public add(isValid: Validate<Value>, message: Message) {
-    this.validators.push({ isValid, message });
+  public add(isValid: Validate<Input>, message: Message) {
+    this.validators.push({ validate: isValid, message });
     return this;
   }
 
-  public create({ required }: ValidationOption) {
+  public create(option: ValidationOption) {
     const { isEmpty, displayName, validators } = this;
 
     // eslint-disable-next-line complexity
-    return (value: Nullable<Value>): Either<ValidationError, true> => {
+    return (value: Nullable<Input>): Either<ValidationError, true> => {
       if (isEmpty(value)) {
-        if (!required) {
+        if (!option?.required) {
           return right(true);
         }
         const message = replaceMessage(VALIDATION_ERROR_MESSAGE.isEmpty, { displayName });
         return failed(value, message);
       }
-      for (const { isValid, message } of validators) {
-        if (!isValid(value)) {
+      for (const { validate, message } of validators) {
+        if (!validate(value, option)) {
           return failed(value, message);
         }
       }

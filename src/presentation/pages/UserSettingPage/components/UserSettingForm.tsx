37a9ec9e0074faf0a91d320d16409c0583a40f69
50ra4 <50ra4.dev.dev@gmail.com>
2021-10-31 23:15:13 +0900

    apply: RestTimeFormGroup to UserSettingForm

--- src/presentation/pages/UserSettingPage/components/UserSettingForm.tsx
+++ src/presentation/pages/UserSettingPage/components/UserSettingForm.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import { TimeRangeForm } from 'presentation/components/forms/TimeRangeForm/TimeRangeForm';
 import { MinuteForm } from 'presentation/components/forms/MinuteForm/MinuteForm';
-import { Range, Time } from 'types';
+import { Range, RestTime, Time } from 'types';
 import { useFormGroup } from 'hooks/useFormGroup';
+import { RestTimeFormGroup } from 'presentation/components/unique/RestTimeFormGroup/RestTimeFormGroup';
 
 type UserSettingFormData = {
   workingHours: Range<Time>;
   roundDownMinutes: number;
+  restTimes: RestTime[];
 };
 
 const INITIAL_USER_SETTING_FORM: UserSettingFormData = {
   workingHours: {},
   roundDownMinutes: 15,
+  restTimes: [],
 };
 
 type Props = {
@@ -22,7 +25,7 @@ type Props = {
 };
 
 export const UserSettingForm = React.memo(function UserSettingForm({ className, readOnly, inline = false }: Props) {
-  const { formState, onChangeFormState } = useFormGroup(INITIAL_USER_SETTING_FORM);
+  const { formState, onChangeFormState } = useFormGroup({ ...INITIAL_USER_SETTING_FORM });
 
   return (
     <StyledRoot className={className}>
@@ -53,6 +56,22 @@ export const UserSettingForm = React.memo(function UserSettingForm({ className, 
         error={undefined} // FIXME:
         onChange={(v) => {
           onChangeFormState('roundDownMinutes', v);
+        }}
+        onClear={() => {
+          onChangeFormState('roundDownMinutes', 0);
+        }}
+      />
+      <RestTimeFormGroup
+        type="input"
+        value={formState.restTimes}
+        readOnly={readOnly}
+        disabled={readOnly}
+        inline={inline}
+        errors={[]} // FIXME:
+        sortable={true}
+        max={5}
+        onChange={(value) => {
+          onChangeFormState('restTimes', value);
         }}
       />
     </StyledRoot>

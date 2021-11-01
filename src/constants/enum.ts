ff79b7508287eaaa0ddabee349aadd7f091062e6
50ra4 <50ra4.dev.dev@gmail.com>
2021-11-01 22:06:55 +0900

    add: DAY_OF_WEEK_LABEL_JP_ENUM

--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -19,3 +19,13 @@ export const DAY_OF_WEEK_ENUM = {
   friday: 5,
   saturday: 6,
 } as const;
+
+export const DAY_OF_WEEK_LABEL_JP_ENUM = {
+  sunday: '日曜日',
+  monday: '月曜日',
+  tuesday: '火曜日',
+  wednesday: '水曜日',
+  thursday: '木曜日',
+  friday: '金曜日',
+  saturday: '土曜日',
+} as const;

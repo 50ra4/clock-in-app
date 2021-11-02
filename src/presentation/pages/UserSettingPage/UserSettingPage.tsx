a3f720d42a1a56ac43f48c86ea4962076cd3e0e9
50ra4 <50ra4.dev.dev@gmail.com>
2021-11-03 00:56:58 +0900

    apply: useUserPreference to UserSettingPage

--- src/presentation/pages/UserSettingPage/UserSettingPage.tsx
+++ src/presentation/pages/UserSettingPage/UserSettingPage.tsx
@@ -1,4 +1,8 @@
-import { useLoginRedirection } from 'hooks/useAuthentication';
+import { useParams } from 'react-router';
+import { useAuthentication, useLoginRedirection } from 'hooks/useAuthentication';
+import { useUserPreference } from 'hooks/useUserPreference';
+
+import { LoadingGuard } from 'presentation/components/feedback/LoadingGuard/LoadingGuard';
 import { ResponsiveLayout } from 'presentation/layouts/ResponsiveLayout/ResponsiveLayout';
 import { WithHeaderLayout } from 'presentation/layouts/WithHeaderLayout/WithHeaderLayout';
 import { TimecardPreferenceView } from './TimecardPreferenceView';
@@ -6,10 +10,23 @@ import { TimecardPreferenceView } from './TimecardPreferenceView';
 const UserSettingPage = (): JSX.Element => {
   useLoginRedirection();
 
+  const { uid } = useParams<{ uid: string }>();
+  const { loggedInUid } = useAuthentication();
+  const { userPreference, isFetching, saveTimeCardPreference } = useUserPreference(uid);
+
+  const isLoggedInUser = loggedInUid === uid;
+
   return (
     <WithHeaderLayout>
       <ResponsiveLayout>
-        <TimecardPreferenceView readOnly={false} inline={true} />
+        <LoadingGuard open={isFetching} />
+        {userPreference && (
+          <TimecardPreferenceView
+            readOnly={!isLoggedInUser}
+            preference={userPreference.timecard}
+            onSave={saveTimeCardPreference}
+          />
+        )}
       </ResponsiveLayout>
     </WithHeaderLayout>
   );

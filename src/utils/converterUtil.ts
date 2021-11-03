33498cd1ba7f4e40ddb18dce5b725aa67cf0f880
50ra4 <50ra4.dev.dev@gmail.com>
2021-11-03 17:51:38 +0900

    add: dailyTimeRecordToRemarks

--- src/utils/converterUtil.ts
+++ src/utils/converterUtil.ts
@@ -1,2 +1,18 @@
+import { DailyTimeRecord } from 'types';
+import { timeToTimeString } from './timeUtil';
+
 export const omitUndefinedProps = <T extends Record<string, unknown>>(obj: T) =>
   Object.fromEntries(Object.entries(obj).filter(([_, value]) => typeof value !== 'undefined')) as T;
+
+export const dailyTimeRecordToRemarks = (
+  { remarks, inHouseWorks }: DailyTimeRecord,
+  separator: string = '\n',
+): string => {
+  const inHouseWorkRemarks = inHouseWorks
+    .map(({ start, end, remarks }) => {
+      const time = [timeToTimeString(start) ?? 'hh:mm', timeToTimeString(end) ?? 'hh:mm'].join('-');
+      return [time, remarks].join(' ');
+    })
+    .join(separator);
+  return [inHouseWorkRemarks, remarks].join(separator);
+};

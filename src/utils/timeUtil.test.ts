0d6b3c356f94363ef4f68c2d0ff62e01000e9991
50ra4 <50ra4.dev.dev@gmail.com>
2021-11-03 17:26:58 +0900

    add: timeRangeToMinute test case

---
+++ src/utils/timeUtil.test.ts
@@ -0,0 +1,9 @@
+import { timeRangeToMinute } from './timeUtil';
+
+describe('timeUtil', () => {
+  describe('timeRangeToMinute', () => {
+    it.todo('should return Number.Nan if time range is empty');
+    it.todo('should return minute');
+    it.todo('should return value added for 24 hours is added if the start hour is greater than the end hour');
+  });
+});

ce529ac7f8ab4d3ae2d77e398c7ef045874d5094
50ra4 <50ra4.dev.dev@gmail.com>
2021-11-14 22:39:25 +0900

    add: converter for document data

--- src/services/converter.ts
+++ src/services/converter.ts
@@ -1,5 +1,7 @@
-import { DailyTimeRecord, InHouseWork, RestTime } from 'types';
+import { DailyTimeRecord, InHouseWork, RestTime, TimecardUserPreference } from 'types';
+import { omitUndefinedProps } from 'utils/converterUtil';
 import { firebase } from './firebase';
+import { createAdditionalProps, formatTimeToQuery } from './utils';
 
 type Query =
   | firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>
@@ -15,6 +17,16 @@ export const queryToRestTime = (query: Query): RestTime =>
     createdAt: query.get('createdAt'),
   } as RestTime);
 
+export const restTimeToDocument = (
+  { id, start, end }: RestTime,
+  { uid, index }: { isUpdated: boolean; uid: string; index: number },
+) => ({
+  order: index + 1,
+  start: formatTimeToQuery(start),
+  end: formatTimeToQuery(end),
+  ...createAdditionalProps(uid, !!id),
+});
+
 export const queryToInHouseWork = (query: Query): InHouseWork =>
   ({
     id: query.id,
@@ -25,6 +37,17 @@ export const queryToInHouseWork = (query: Query): InHouseWork =>
     updatedAt: query.get('updatedAt'),
     createdAt: query.get('createdAt'),
   } as InHouseWork);
+
+export const inHouseWorkToDocument = (
+  { id, start, end, remarks }: InHouseWork,
+  { uid, index }: { uid: string; index: number },
+) => ({
+  order: index + 1,
+  start: formatTimeToQuery(start),
+  end: formatTimeToQuery(end),
+  remarks,
+  ...createAdditionalProps(uid, !!id),
+});
 
 export const queryToDailyTimeRecord = (query: Query): DailyTimeRecord =>
   ({
@@ -39,14 +62,45 @@ export const queryToDailyTimeRecord = (query: Query): DailyTimeRecord =>
     createdAt: query.get('createdAt'),
   } as DailyTimeRecord);
 
+export const dailyTimeRecordToDocument = (
+  { start, end, remarks }: DailyTimeRecord,
+  { uid, isUpdated }: { uid: string; isUpdated: boolean },
+) => ({
+  start: formatTimeToQuery(start),
+  end: formatTimeToQuery(end),
+  remarks,
+  ...createAdditionalProps(uid, isUpdated),
+});
+
 export const queryToTimecardUserPreference = (query: Query) => ({
   workingStart: query.get('workingStart'),
   workingEnd: query.get('workingEnd'),
   roundDownMinute: query.get('roundDownMinute'),
   regularHolidays: query.get('regularHolidays'),
+  // FIXME:
   lunchRestTime: query.get('lunchRestTime'),
   restTimes: [], // NOTE: fetch sub-collection
   // FIXME: TimecardUserPreference type
   updatedAt: query.get('updatedAt'),
   createdAt: query.get('createdAt'),
+});
+
+export const timecardUserPreferenceToDocument = (
+  {
+    workingTimes: { start: workingStart, end: workingEnd },
+    lunchRestTime: { start: lunchRestTimeStart, end: lunchRestTimeEnd },
+    regularHolidays,
+    roundDownMinute,
+  }: TimecardUserPreference,
+  { uid, isUpdated }: { uid: string; isUpdated: boolean },
+) => ({
+  workingStart: formatTimeToQuery(workingStart),
+  workingEnd: formatTimeToQuery(workingEnd),
+  // FIXME:
+  lunchRestTime: omitUndefinedProps({ lunchRestTimeStart, lunchRestTimeEnd }),
+  // lunchRestTimeStart: formatTimeToQuery(lunchRestTimeStart),
+  // lunchRestTimeEnd: formatTimeToQuery(lunchRestTimeEnd),
+  regularHolidays,
+  roundDownMinute,
+  ...createAdditionalProps(uid, isUpdated),
 });
